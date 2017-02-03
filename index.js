bbf6d59b73a403bd6863e29713419067f48be196
Cristopher <yusuke_868@hotmail.com>
2017-02-03 13:34:46 -0200

    teste without liveserver

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ var router = express();
 router.get('/webhook',function(req,res){
   res.send('ok')
 })
+
+router.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+router.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
